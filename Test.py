07bc3a2b47816a9547c7daf57eb0f56937e9164f
Meme Lord <nhialjc20@g.eips.ca>
2018-05-31 00:25:14 -0600

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,8 +2,14 @@ from flask import Flask, request, redirect
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 import random
-
 app = Flask(__name__)
+import json
+import couchdb
+from re import *
+user = "MrCoco"
+password = "Nhialiscool123"
+couchserver = couchdb.Server("http://%s:%s@localhost:5984/" % (user, password))
+# Make quote
 def get_quote():
     r = requests.get("https://frinkiac.com/api/random")
     if r.status_code == 200:
@@ -19,34 +25,61 @@ def get_quote():
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
+    inbound_user = request.form.get("From")
     inbound_message = request.form.get("Body")
     inbound_message = inbound_message.lower().strip()
     image_url, caption = get_quote()
     """Respond to incoming calls with a MMS message."""
-    repeats = 10
+    f = open("Users.json",'w')
+    dbname = 'user' + inbound_user
+    if dbname in couchserver:
+        db = couchserver[dbname]
+    else:
+        db = couchserver.create(dbname)
+        db = couchserver[dbname]
     if inbound_message == 'send':
-        rareity = random.randint(1,10)
+        rareity = random.randint(1,50)
         resp = MessagingResponse()
         msg = resp.message(caption)
         msg.media(image_url)
+        
+        # Common Simpson
         if rareity == 1 | 2 | 3:
             resp = MessagingResponse()
             msg = resp.message(caption + "\n\nThis is a Common Simpson")
             msg.media(image_url)
+            1
+            # Uncommon Simpson
         elif rareity == 4 | 5 | 6 | 7:
             resp = MessagingResponse()
             msg = resp.message(caption + "\n\nThis is a Uncommon Simpson")
             msg.media(image_url)
-        elif rareity == 8 | 9:
+            
+            # Epic Simpson
+        elif rareity == 9:
+            msg.media(image_url)
             resp = MessagingResponse()
             msg = resp.message(caption + "\n\nThis is an Epic Simpson")
-        else:
+            
+            # Legendary Simpson
+        elif  rareity == 48 | 49 | 50:
             resp = MessagingResponse()
             msg = resp.message(caption + "\n\nThe is a Legendary Simpson")
             msg.media(image_url)
+            
+            # Random Simpson
+        else:
+            randomz = ["\n\nThis is a Uncommon Simpson","\n\nThis is a Uncommon Simpson","\n\nThis is an Epic Simpson",]
+            choice = random.choice(randomz)
+            resp = MessagingResponse()
+            msg = resp.message(caption + choice)
+            msg.media(image_url)
+
         return str(resp)
-
-
+    else:
+        resp = MessagingResponse()
+        msg = resp.message("Start by messaging \"Send\"")
+        return str(resp)
 
 
 if __name__ == "__main__":
