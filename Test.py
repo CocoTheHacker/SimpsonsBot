1801246d7c50df385679da0cf5f27dd317821b78
Coco <40618579+CocoTheHacker@users.noreply.github.com>
2018-11-06 09:50:59 -0700

    OMG AN UPDATE!!!!!!
    Database works and you can retrieve your simpsons

--- Test.py
+++ Test.py
@@ -22,65 +22,45 @@ def get_quote():
         # Combine each line of subtitles into one string.
         caption = "\n".join([subtitle["Content"] for subtitle in json["Subtitles"]])
         return image_url, caption
+try:
+    @app.route("/sms", methods=['GET', 'POST'])
+    def sms_reply():
+        inbound_user = request.form.get("From")
+        inbound_message = request.form.get("Body")
+        inbound_message = inbound_message.lower().strip()
+        image_url, caption = get_quote()
+        """Respond to incoming calls with a MMS message."""
+        f = open("Users.json",'w')
 
-@app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
-    inbound_user = request.form.get("From")
-    inbound_message = request.form.get("Body")
-    inbound_message = inbound_message.lower().strip()
-    image_url, caption = get_quote()
-    """Respond to incoming calls with a MMS message."""
-    f = open("Users.json",'w')
-    dbname = 'user' + inbound_user
-    if dbname in couchserver:
-        db = couchserver[dbname]
-    else:
-        db = couchserver.create(dbname)
-        db = couchserver[dbname]
-    if inbound_message == 'send':
-        rareity = random.randint(1,50)
-        resp = MessagingResponse()
-        msg = resp.message(caption)
-        msg.media(image_url)
-        
-        # Common Simpson
-        if rareity == 1 | 2 | 3:
-            resp = MessagingResponse()
-            msg = resp.message(caption + "\n\nThis is a Common Simpson")
-            msg.media(image_url)
-            1
-            # Uncommon Simpson
-        elif rareity == 4 | 5 | 6 | 7:
-            resp = MessagingResponse()
-            msg = resp.message(caption + "\n\nThis is a Uncommon Simpson")
-            msg.media(image_url)
-            
-            # Epic Simpson
-        elif rareity == 9:
-            msg.media(image_url)
-            resp = MessagingResponse()
-            msg = resp.message(caption + "\n\nThis is an Epic Simpson")
-            
-            # Legendary Simpson
-        elif  rareity == 48 | 49 | 50:
-            resp = MessagingResponse()
-            msg = resp.message(caption + "\n\nThe is a Legendary Simpson")
-            msg.media(image_url)
-            
-            # Random Simpson
+        dbname = 'user' + inbound_user
+        if dbname in couchserver:
+            db = couchserver[dbname]
         else:
-            randomz = ["\n\nThis is a Uncommon Simpson","\n\nThis is a Uncommon Simpson","\n\nThis is an Epic Simpson",]
+            db = couchserver.create(dbname)
+            db = couchserver[dbname]
+
+        if inbound_message == 'send':
+            resp = MessagingResponse()
+            msg = resp.message(caption)
+            msg.media(image_url)
+
+            randomz = ["\n\nThis is a Uncommon Simpson","\n\nThis is a Common Simpson","\n\nThis is an Epic Simpson","\n\nThis is a Legendary Simpson"]
+            number_gen = random.randint(1,1000)
             choice = random.choice(randomz)
             resp = MessagingResponse()
-            msg = resp.message(caption + choice)
+            msg = resp.message(caption + choice + '\n' + str(number_gen))
             msg.media(image_url)
+            db["simpson" +"-"+str(number_gen)] = {"id": image_url,"id2": caption}
 
-        return str(resp)
-    else:
-        resp = MessagingResponse()
-        msg = resp.message("Start by messaging \"Send\"")
-        return str(resp)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+            return str(resp)
+        else:
+            resp = MessagingResponse()
+            msg = resp.message("Start by messaging \"Send\"")
+            return str(resp)
+    @app.route("/")
+    def index():
+        return "Hello World"
+    if __name__ == "__main__":
+        app.run(debug=True)
+except Exception as e:
+    print(e)
